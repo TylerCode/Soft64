0d4b37345eabd29babcc9ba96ec44aa5ff230e55
bryanperris <bryanperris@live.com>
2015-10-10 01:30:42 -0400

    Add some missing files.

---
+++ Source/Soft64WPF/ViewModels/Register64Value.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace Soft64WPF.ViewModels
+{
+    public sealed class Register64Value : RegisterValueViewModel
+    {
+        public Register64Value()
+        {
+            Value = "0000000000000000";
+        }
+
+        protected override string MakeString(dynamic value)
+        {
+            UInt64 _value = value;
+            return _value.ToString("X16");
+        }
+
+        protected override dynamic ParseValue(string value) =>
+            UInt64.Parse(value, NumberStyles.AllowHexSpecifier, CultureInfo.CurrentCulture);
+
+        protected override bool ValueCheck(string value)
+        {
+            UInt64 _value = 0;
+            return UInt64.TryParse(value, out _value);
+        } 
+    }
+}
---
+++ Source/Soft64WPF/ViewModels/RegisterFPUValue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace Soft64WPF.ViewModels
+{
+    public sealed class RegisterFPUValue : RegisterValueViewModel
+    {
+        /* TODO: Use tagging support to switch value mode of FPU registers */
+
+        public RegisterFPUValue()
+        {
+            Value = "0.000000";
+        }
+
+        protected override string MakeString(dynamic value)
+        {
+            Double d = value;
+            return d.ToString();
+        }
+
+        protected override dynamic ParseValue(string value)
+        {
+            return Double.Parse(value, NumberStyles.AllowDecimalPoint, CultureInfo.CurrentCulture);
+        }
+
+        protected override bool ValueCheck(string value)
+        {
+            Double d;
+            return Double.TryParse(value, out d);
+        }
+    }
+}
