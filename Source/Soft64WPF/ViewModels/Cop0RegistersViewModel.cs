a7a08b9d8c1b2396f7fced7ac18436894b3a19d6
bryanperris <bryanperris@live.com>
2015-10-11 01:52:30 -0400

    More work on Cop0 view model.

--- Source/Soft64WPF/ViewModels/Cop0RegistersViewModel.cs
+++ Source/Soft64WPF/ViewModels/Cop0RegistersViewModel.cs
@@ -36,5 +36,31 @@ namespace Soft64WPF.ViewModels
         public static readonly DependencyProperty CompareProperty = ComparePK.DependencyProperty;
         public Register32Value Compare => GetValue(CompareProperty);
         #endregion
+
+        #region DP - Bad Virtual Address
+        private static readonly DependencyPropertyKey BadVAddressPK = RegDPKey<Cop0RegistersViewModel, Register64Value>("BadVAddress");
+        public static readonly DependencyProperty BadVAddressProperty = BadVAddressPK.DependencyProperty;
+        public Register64Value BadVAddress => GetValue(BadVAddressProperty);
+        #endregion
+
+        #region DP - Entry Hi
+        private static readonly DependencyPropertyKey EntryHiPK = RegDPKey<Cop0RegistersViewModel, Register64Value>("EntryHi");
+        public static readonly DependencyProperty EntryHiProperty = EntryHiPK.DependencyProperty;
+        public Register64Value EntryHi => GetValue(EntryHiProperty);
+        #endregion
+
+        #region DP - Entry Lo 0
+        private static readonly DependencyPropertyKey EntryLo0PK = RegDPKey<Cop0RegistersViewModel, Register64Value>("EntryLo0");
+        public static readonly DependencyProperty EntryLo0Property = EntryLo0PK.DependencyProperty;
+        public Register64Value EntryLo0 => GetValue(EntryLo0Property);
+        #endregion
+
+        #region DP - Entry Lo 1
+        private static readonly DependencyPropertyKey EntryLo1PK = RegDPKey<Cop0RegistersViewModel, Register64Value>("EntryLo1");
+        public static readonly DependencyProperty EntryLo1Property = EntryLo1PK.DependencyProperty;
+        public Register64Value EntryLo1 => GetValue(EntryLo1Property);
+        #endregion
+
+
     }
 }
