7c5df261b94536bbf6258f0ba4544df4ed9802f4
bryanperris <bryanperris@live.com>
2015-10-21 16:34:41 -0400

    Implemented basic support of loading cartridge files.

--- Source/Soft64UI/ChromeHostForm.cs
+++ Source/Soft64UI/ChromeHostForm.cs
@@ -9,19 +9,18 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using CefSharp.WinForms;
 using CefSharp;
+using Soft64;
+using System.IO;
 
 namespace Soft64UI
 {
     public partial class ChromeHostForm : Form
     {
         private ChromiumWebBrowser m_HostBrowser;
-        private DebugCallbacks m_DebugCallbacks = new DebugCallbacks();
 
         public ChromeHostForm()
         {
             InitializeComponent();
-
-            m_DebugCallbacks.ShowTools += M_DebugCallbacks_ShowTools;
         }
 
         protected override void OnLoad(EventArgs e)
@@ -35,19 +34,37 @@ namespace Soft64UI
             /* Create an instance of the Chrome browser host */
             m_HostBrowser = new ChromiumWebBrowser($"{Environment.CurrentDirectory}/HTMLUI/Start.html");
             Controls.Add(m_HostBrowser);
-            m_HostBrowser.LoadingStateChanged += M_HostBrowser_LoadingStateChanged;
 
             /* Register JS Objects */
-            m_HostBrowser.RegisterJsObject("debugCallbacks", m_DebugCallbacks);
+            m_HostBrowser.RegisterJsObject("mainWindow", this);
+
+            Machine.Current.DeviceRCP.Interface_Parallel.CartridgeChanged += Interface_Parallel_CartridgeChanged;
         }
 
-        private void M_DebugCallbacks_ShowTools(object sender, EventArgs e)
+        private void Interface_Parallel_CartridgeChanged(object sender, Soft64.RCP.CartridgeChangedEventArgs e)
+        {
+            m_HostBrowser.ExecuteScriptAsync(
+                $@"$('#cartrigeInfo').html(' \
+                    <b>Name: {e.NewCartridge?.RomImage.Name} </b> \
+                    <br /> \
+                ');");
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+        public void InsertRomFile(String stringBuffer)
+        {
+            Byte[] buffer = Convert.FromBase64String(stringBuffer);
+            VirtualCartridge cart = new VirtualCartridge(new MemoryStream(buffer));
+            Machine.Current.CartridgeInsert(cart);
+        }
+
+        public void ShowDevTools()
         {
             m_HostBrowser.ShowDevTools();
-        }
-
-        private void M_HostBrowser_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
-        {
         }
     }
 }
