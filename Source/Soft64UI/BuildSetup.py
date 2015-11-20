116b500518e1272508d4d2eae8860e6d8ec874fb
bryanperris <bryanperris@live.com>
2015-11-20 15:36:35 -0500

    More work on python based build script.

--- Source/Soft64UI/BuildSetup.py
+++ Source/Soft64UI/BuildSetup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import sys
 import os
+import re
 
 print ("Setting up resources for Soft64UI")
 
@@ -11,10 +12,28 @@ if os.path.isdir(sys.argv[1]) != True:
 	raise ValueError('Binary output command switch points to invalid path')
 	
 dirBinaryOutput = sys.argv[1]
-releaseType = sys.argv[0]
+releaseType = sys.argv[0].lower()
+
+isDebug = releaseType is "debug"
 
 # Get working directory
 workingDir = os.path.dirname(os.path.realpath(__file__))
 htmluiDir = workingDir + "\\HTMLUI"
 
+fileCopyList = []
 
+# Walk through each file of the source HTMLUI directory
+for dirName, subdirList, fileList in os.walk(rootDir):
+	for fname in fileList:
+        fileCopyList.append(fname)
+
+#process items in the list
+for file in fileCopyList:
+	fileName = os.path.splitext(file)[0]
+	fileBase = os.path.basename(file)
+	fileExt = fileBase[fileBase.rfind("."):]
+	print("Processing File: " + fileName)
+	if isDebug and fileBase.match(".min."):
+		orginFile = file.replace(".min.", "")
+		if orginFile in fileCopyList:
+			fileCopyList.remove(orginFile)
