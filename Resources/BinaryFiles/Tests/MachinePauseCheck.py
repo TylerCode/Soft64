760c117fba1e3fe48451fa0022716cdf72a4d77a
bryanperris <bryanperris@live.com>
2015-12-14 15:29:45 -0500

    Script fix to use WinForms message box API and not WPF one.

--- Resources/BinaryFiles/Tests/MachinePauseCheck.py
+++ Resources/BinaryFiles/Tests/MachinePauseCheck.py
@@ -1,5 +1,5 @@
 from Soft64 import Machine
 from System import String
-from System.Windows import MessageBox
+from System.Windows.Forms import MessageBox
 
 MessageBox.Show(String.Format("Machine Paused: {0}", Machine.Current.IsPaused == True))
