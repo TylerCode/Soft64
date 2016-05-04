5585c4d9b86867ed0697d1fe1a7bb37da32f81ed
bryanperris <bryanperris@live.com>
2016-05-04 11:16:48 -0400

    Remove the electron source.

--- Source/Soft64Quantum/electron/lib/browser/api/power-monitor.js
+++
@@ -1,6 +0,0 @@
-const EventEmitter = require('events').EventEmitter
-const powerMonitor = process.atomBinding('power_monitor').powerMonitor
-
-Object.setPrototypeOf(powerMonitor, EventEmitter.prototype)
-
-module.exports = powerMonitor
--- Source/Soft64Quantum/electron/lib/browser/api/screen.js
+++
@@ -1,6 +0,0 @@
-const EventEmitter = require('events').EventEmitter
-const screen = process.atomBinding('screen').screen
-
-Object.setPrototypeOf(screen, EventEmitter.prototype)
-
-module.exports = screen
