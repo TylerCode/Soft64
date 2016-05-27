6c3b8170ce085e6a573b213c053c3160e3b12516
bryanperris <bryanperris@live.com>
2016-05-27 13:25:57 -0400

    New base for the soft64 emulator core

---
+++ Soft64CoreLibraries/Soft64.CoreMachine/Interfaces.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soft64.CoreMachine
+{
+    /// <summary>
+    /// Main interface a for an instance of a Soft64 emulator core
+    /// </summary>
+    /// <remarks>
+    /// This interface provides the main API for the Soft64 emulator core.
+    /// Emulator configuration and controls, and state are exposed through this
+    /// type of interface.
+    /// </remarks>
+    public interface ISoft64
+    {
+        /// <summary>
+        /// Start up the emulator core on the current thread calling this method
+        /// </summary>
+        void Run();
+
+        /// <summary>
+        /// Start up the emulator core in an asynchronous matter.
+        /// </summary>
+        void RunAsync();
+    }
+
+    public interface ISoft64Result
+    {
+
+    }
+
+    public interface ISoft64Error
+    {
+        String Message { get; }
+        Int32 Code { get; }
+
+    }
+}
