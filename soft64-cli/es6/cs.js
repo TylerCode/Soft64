0fcceb883a89ac786b03afd48552e109b513145e
bryanperris <bryanperris@live.com>
2016-06-03 13:27:08 -0400

    Print out the assembly version string, it works on Windows so far.

--- soft64-cli/es6/cs.js
+++ soft64-cli/es6/cs.js
@@ -18,7 +18,7 @@ class cs {
           public async Task<Object> Invoke(Object input)
           {
             Assembly coreAssembly = Assembly.LoadFrom("Soft64/Soft64.CoreMachine.dll");
-           return String.Format("Result of load: {0}", coreAssembly != null);
+           return String.Format("Result of load: {0}", coreAssembly.ToString());
           }
         }
       */
